08b7b12fcee56752b45fbd9f3be7071dee3a979b
Yun Zheng Hu <hu@fox-it.com>
2022-08-22 15:42:19 +0200

    Refactored elasticsearch adapter into working state
    Example usage:
    
    ```shell
    # Write example records to ES via http (default is https)
    $ rdump jsonfile://examples/records.json -w 'elastic+http://127.0.0.1:9200?index=pastebin'
    
    # Read records from ES with index pastebin
    $ rdump -L 'elastic+http://127.0.0.1:9200?index=pastebin'
    --[ RECORD 1 ]--
                key = Q42eWSaF
               date = 2019-03-19T09:09:47.000000
        expire_date = 1970-01-01T00:00:00.000000
              title = A sample pastebin record
            content = This is the content of a sampe pastebin record
               user =
             syntax = text
            _source = None
    _classification = None
         _generated = 2022-08-22 13:37:47.127217
           _version = 1
    ```

--- flow/record/adapter/elastic.py
+++ flow/record/adapter/elastic.py
@@ -1,43 +1,103 @@
+import threading
+import logging
+import queue
+
 import elasticsearch
 import elasticsearch.helpers
 
 from flow.record.adapter import AbstractWriter, AbstractReader
+from flow.record import JsonRecordPacker, RecordDescriptor
+from flow.record.fieldtypes import fieldtype_for_value
 
-
-def index_stream(index, it):
-    for r in it:
-        d = r.dict()
-        if "Value" in d:
-            del d["Value"]
-
-        yield {
-            "_index": index,
-            "_type": "event_" + str(d["EventID"]),
-            "_source": d,
-        }
+log = logging.getLogger(__name__)
 
 
 class ElasticWriter(AbstractWriter):
-
-    def __init__(self, index, **kwargs):
+    def __init__(self, uri, index="records", http_compress=True, **kwargs):
         self.index = index
+        self.uri = uri
+        http_compress = str(http_compress).lower() in ("1", "true")
+        self.es = elasticsearch.Elasticsearch(uri, http_compress=http_compress)
+        self.json_packer = JsonRecordPacker()
+        self.queue = queue.Queue()
+        self.event = threading.Event()
+        self.thread = threading.Thread(target=self.streaming_bulk_thread).start()
 
-        self.es = elasticsearch.Elasticsearch()
+    def record_to_document(self, record, index):
+        """Convert a record to a Elasticsearch compatible document dictionary"""
+        rdict = record._asdict()
 
-    # def writeblob(self, src):
-    #   count = elasticsearch.helpers.bulk(es, index_stream("logtest", src))
+        # Store record metadata under `_record_metadata`
+        rdict_meta = {
+            "descriptor": {
+                "name": record._desc.name,
+                "hash": record._desc.descriptor_hash,
+            },
+        }
+        # Move all dunder fields to `_record_metadata` to avoid naming clash with ES.
+        dunder_keys = [key for key in rdict if key.startswith("_")]
+        for key in dunder_keys:
+            rdict_meta[key.lstrip("_")] = rdict.pop(key)
+        rdict["_record_metadata"] = rdict_meta
+
+        document = {
+            "_index": index,
+            "_source": self.json_packer.pack(rdict),
+        }
+        return document
+
+    def document_stream(self):
+        """Generator of record documents on the Queue"""
+        while True:
+            record = self.queue.get()
+            if record == StopIteration:
+                break
+            yield self.record_to_document(record, index=self.index)
+
+    def streaming_bulk_thread(self):
+        """Thread that streams the documents to ES via the bulk api"""
+        for ok, item in elasticsearch.helpers.streaming_bulk(
+            self.es,
+            self.document_stream(),
+            raise_on_error=False,
+            raise_on_exception=False,
+        ):
+            if not ok:
+                log.error(f"Failed to insert {item}")
+        self.event.set()
 
     def write(self, r):
-        self.es.index({"_index": self.index, "_type": r._desc.name, "_source": r.dict()})
+        self.queue.put_nowait(r)
 
     def flush(self):
         pass
 
     def close(self):
-        pass
+        self.queue.put(StopIteration)
+        self.event.wait()
+        self.es.close()
 
 
 class ElasticReader(AbstractReader):
+    def __init__(self, uri, index="records", http_compress=True, selector=None, **kwargs):
+        self.index = index
+        self.uri = uri
+        self.selector = selector
+        http_compress = str(http_compress).lower() in ("1", "true")
+        self.es = elasticsearch.Elasticsearch(uri, http_compress=http_compress)
 
-    def __iter__(self, r, **kwargs):
-        raise NotImplementedError()
+    def __iter__(self):
+        res = self.es.search(index=self.index)
+        log.debug(f"ElasticSearch returned {res['hits']['total']['value']} hits")
+        for hit in res["hits"]["hits"]:
+            source = hit["_source"]
+            if "_record_metadata" in source:
+                _ = source.pop("_record_metadata")
+            fields = [(fieldtype_for_value(val, "string"), key) for key, val in source.items()]
+            desc = RecordDescriptor("elastic/record", fields)
+            obj = desc(**source)
+            if not self.selector or self.selector.match(obj):
+                yield obj
+
+    def close(self):
+        self.es.close()
