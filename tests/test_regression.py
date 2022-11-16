fox-it/flow.record

commit 083e3b25ff2b2e4985dafe795fbda7e9fb803d9f

Add hex format specifiers to `bytes` fieldtype
This allows for using a hex format specifier to the bytes fieldtype
when using a format string, for example when using `rdump`:

  $ rdump -f "{bytes_field:x}"

Other related `hex` format specifiers are:

  :x -- hex lowercase
  :X -- hex uppercase
  :hex -- hex lowercase
  :HEX -- hex uppercase
  :#x -- hex lowercase with 0x prefix
  :#X -- hex uppercase with 0x prefix

(DIS-1640)

flow/record/fieldtypes/__init__.py
@@ -214,6 +214,17 @@ class bytes(bytes_type, FieldType):
     def __repr__(self):
         return repr(self.value)
 
+    def __format__(self, spec):
+        if spec in ("hex", "x"):
+            return self.hex()
+        elif spec in ("HEX", "X"):
+            return self.hex().upper()
+        elif spec in ("#x"):
+            return "0x" + self.hex()
+        elif spec in ("#X"):
+            return "0x" + self.hex().upper()
+        return bytes_type.__format__(self, spec)
+
 
 class datetime(_dt, FieldType):
     def __new__(cls, *args, **kwargs):

tests/test_fieldtypes.py
@@ -621,5 +621,30 @@ def test_format_defang(record_type, value, expected):
     assert f"{record.value:>100}" == f"{value:>100}"
 
 
+@pytest.mark.parametrize(
+    "spec,value,expected",
+    [
+        ("x", b"\xac\xce\x55\xed", "acce55ed"),
+        ("X", b"\xac\xce\x55\xed", "ACCE55ED"),
+        ("#x", b"\xac\xce\x55\xed", "0xacce55ed"),
+        ("#X", b"\xac\xce\x55\xed", "0xACCE55ED"),
+        ("hex", b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e", "000102030405060708090a0b0c0d0e"),
+        ("HEX", b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e", "000102030405060708090A0B0C0D0E"),
+        ("x", b"", ""),
+    ],
+)
+def test_format_hex(spec, value, expected):
+    TestRecord = RecordDescriptor(
+        "test/format/hex",
+        [
+            ("bytes", "value"),
+        ],
+    )
+
+    record = TestRecord(value)
+    format_str = "{:" + spec + "}"
+    assert format_str.format(record.value) == expected
+
+
 if __name__ == "__main__":
     __import__("standalone_test").main(globals())

tests/test_rdump.py
@@ -244,3 +244,45 @@ def test_rdump_json_no_descriptors(tmp_path):
         assert json_dict["digest"]["md5"] == hashlib.md5(data).hexdigest()
         assert json_dict["digest"]["sha1"] == hashlib.sha1(data).hexdigest()
         assert json_dict["digest"]["sha256"] == hashlib.sha256(data).hexdigest()
+
+
+def test_rdump_format_spec_hex(tmp_path):
+    TestRecord = RecordDescriptor(
+        "test/record",
+        [
+            ("bytes", "data"),
+        ],
+    )
+
+    # generate a test record
+    test_record = TestRecord(
+        data=b"\x00\x01--hello world--\xee\xff",
+    )
+
+    # write the test record so rdump can read it
+    record_path = tmp_path / "test.records"
+    with RecordWriter(record_path) as writer:
+        writer.write(test_record)
+
+    # rdump with --format string using our hex format spec
+    args = [
+        "rdump",
+        str(record_path),
+        "--format",
+        "hex:{data:hex} HEX:{data:HEX} x:{data:x} X:{data:X} #x:{data:#x} #X:{data:#X}",
+    ]
+    process = subprocess.Popen(args, stdout=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+
+    assert process.returncode == 0
+    assert stderr is None
+    assert stdout.rstrip() == b" ".join(
+        [
+            b"hex:00012d2d68656c6c6f20776f726c642d2deeff",
+            b"HEX:00012D2D68656C6C6F20776F726C642D2DEEFF",
+            b"x:00012d2d68656c6c6f20776f726c642d2deeff",
+            b"X:00012D2D68656C6C6F20776F726C642D2DEEFF",
+            b"#x:0x00012d2d68656c6c6f20776f726c642d2deeff",
+            b"#X:0x00012D2D68656C6C6F20776F726C642D2DEEFF",
+        ]
+    )

commit 63c979d1b0f97d9f0df8299bc9d097369d83b51d

Fixed bytes output when using LineAdapter
Bug was introduced in 083e3b25ff2b2e4985dafe795fbda7e9fb803d9f
Due to incorrect `in` operation, bytes would be printed as hex strings.

flow/record/fieldtypes/__init__.py
@@ -219,9 +219,9 @@ class bytes(bytes_type, FieldType):
             return self.hex()
         elif spec in ("HEX", "X"):
             return self.hex().upper()
-        elif spec in ("#x"):
+        elif spec == "#x":
             return "0x" + self.hex()
-        elif spec in ("#X"):
+        elif spec == "#X":
             return "0x" + self.hex().upper()
         return bytes_type.__format__(self, spec)
 

tests/test_regression.py
@@ -419,5 +419,20 @@ def test_grouped_replace():
     excinfo.match(".*Got unexpected field names:.*non_existing_field.*")
 
 
+def test_bytes_line_adapter(capsys):
+    TestRecord = RecordDescriptor(
+        "test/bytes_hex",
+        [
+            ("bytes", "data"),
+        ],
+    )
+
+    with RecordWriter("line://") as writer:
+        writer.write(TestRecord(b"hello world"))
+
+    captured = capsys.readouterr()
+    assert "data = b'hello world'" in captured.out
+
+
 if __name__ == "__main__":
     __import__("standalone_test").main(globals())