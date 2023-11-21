98c09e22ea820c063ebfcc766f0efe7d16766b28
Yun Zheng Hu <hu@fox-it.com>
2023-11-21 15:17:26 +0000

    Add DuckDB adapter
    This adds DuckDB reader and writer support. Because DuckDB is mostly
    compatible with the SQLite API, we just subclass from the existing
    SQLite adapter with minimal changes.
    
    Changes done the SQLite adapter and tests are:
    
     * backtick quoting does not work in DuckDB so we use double quotes now
     * DuckDB has strict typing so some tests are not applicable and skipped
     * `executescript()` does not exist in DuckDB so we avoid using it
     * Switched SQLite to `isolation_level=None` for manual transactions

---
+++ flow/record/adapter/duckdb.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import duckdb
+
+from flow.record.adapter.sqlite import (
+    Selector,
+    SqliteReader,
+    SqliteWriter,
+    make_selector,
+)
+
+__usage__ = """
+DuckDB adapter
+---
+Write usage: rdump -w duckdb://[PATH]?batch_size=[BATCH_SIZE]
+Read usage: rdump duckdb://[PATH]?batch_size=[BATCH_SIZE]
+[PATH]: path to DuckDB database file
+
+Optional parameters:
+    [BATCH_SIZE]: number of records to read or write in a single transaction (default: 1000)
+"""
+
+
+class DuckdbReader(SqliteReader):
+    """DuckDB reader, subclasses from SQLite reader."""
+
+    def __init__(self, path: str, *, batch_size: str | int = 1000, selector: Selector | str | None = None, **kwargs):
+        self.selector = make_selector(selector)
+        self.descriptors_seen = set()
+        self.con = duckdb.connect(path)
+        self.count = 0
+        self.batch_size = int(batch_size)
+
+
+class DuckdbWriter(SqliteWriter):
+    """DuckDB writer, subclasses from SQLite writer."""
+
+    def __init__(self, path: str, *, batch_size: str | int = 1000, **kwargs):
+        self.descriptors_seen = set()
+        self.con = None
+        self.con = duckdb.connect(path)
+        self.count = 0
+        self.batch_size = int(batch_size)
+        self.con.begin()
+
+    def tx_cycle(self) -> None:
+        self.con.execute("COMMIT")
+        self.con.execute("BEGIN")
--- flow/record/adapter/sqlite.py
+++ flow/record/adapter/sqlite.py
@@ -11,14 +11,12 @@ from flow.record.adapter import AbstractReader, AbstractWriter
 from flow.record.base import RESERVED_FIELDS, normalize_fieldname
 from flow.record.selector import Selector, make_selector
 
-logger = logging.getLogger(__name__)
-
 __usage__ = """
 SQLite adapter
 ---
 Write usage: rdump -w sqlite://[PATH]?batch_size=[BATCH_SIZE]
 Read usage: rdump sqlite://[PATH]?batch_size=[BATCH_SIZE]
-[PATH]: path to sqlite database file
+[PATH]: path to SQLite database file
 
 Optional parameters:
     [BATCH_SIZE]: number of records to read or write in a single transaction (default: 1000)
@@ -58,12 +56,12 @@ def create_descriptor_table(con: sqlite3.Connection, descriptor: RecordDescripto
     column_defs = []
     for column_name, fieldset in descriptor.get_all_fields().items():
         column_type = FIELD_MAP.get(fieldset.typename, "TEXT")
-        column_defs.append(f"   `{column_name}` {column_type}")
+        column_defs.append(f'   "{column_name}" {column_type}')
     sql_columns = ",\n".join(column_defs)
 
     # Create the descriptor table
-    sql = f"CREATE TABLE IF NOT EXISTS `{table_name}` (\n{sql_columns}\n)"
-    logger.debug(sql)
+    sql = f'CREATE TABLE IF NOT EXISTS "{table_name}" (\n{sql_columns}\n)'
+    logging.debug(sql)
     con.execute(sql)
 
 
@@ -72,7 +70,7 @@ def update_descriptor_columns(con: sqlite3.Connection, descriptor: RecordDescrip
     table_name = descriptor.name
 
     # Get existing columns
-    cursor = con.execute(f"PRAGMA table_info(`{table_name}`)")
+    cursor = con.execute(f'PRAGMA table_info("{table_name}")')
     column_names = set(row[1] for row in cursor.fetchall())
 
     # Add missing columns
@@ -81,23 +79,23 @@ def update_descriptor_columns(con: sqlite3.Connection, descriptor: RecordDescrip
         if column_name in column_names:
             continue
         column_type = FIELD_MAP.get(fieldset.typename, "TEXT")
-        column_defs.append(f"  ALTER TABLE `{table_name}` ADD COLUMN `{column_name}` {column_type}")
+        column_defs.append(f'  ALTER TABLE "{table_name}" ADD COLUMN "{column_name}" {column_type}')
 
     # No missing columns
     if not column_defs:
         return None
 
     # Add the new columns
-    sql = ";\n".join(column_defs)
-    con.executescript(sql)
+    for col_def in column_defs:
+        con.execute(col_def)
 
 
 @lru_cache(maxsize=1000)
 def prepare_insert_sql(table_name: str, field_names: tuple[str]) -> str:
     """Return (cached) prepared SQL statement for inserting a record based on table name and field names."""
-    column_names = ", ".join(f"`{name}`" for name in field_names)
+    column_names = ", ".join(f'"{name}"' for name in field_names)
     value_placeholder = ", ".join(["?"] * len(field_names))
-    return f"INSERT INTO `{table_name}` ({column_names}) VALUES ({value_placeholder})"
+    return f'INSERT INTO "{table_name}" ({column_names}) VALUES ({value_placeholder})'
 
 
 def db_insert_record(con: sqlite3.Connection, record: Record) -> None:
@@ -117,13 +115,15 @@ def db_insert_record(con: sqlite3.Connection, record: Record) -> None:
         values.append(value)
 
     # Insert record into database
-    logger.debug(sql)
-    logger.debug(values)
+    logging.debug(sql)
+    logging.debug(values)
     con.execute(sql, values)
 
 
 class SqliteReader(AbstractReader):
-    def __init__(self, path: str, batch_size: str | int = 1000, selector: Selector | str | None = None, **kwargs):
+    """SQLite reader."""
+
+    def __init__(self, path: str, *, batch_size: str | int = 1000, selector: Selector | str | None = None, **kwargs):
         self.selector = make_selector(selector)
         self.descriptors_seen = set()
         self.con = sqlite3.connect(path)
@@ -141,6 +141,7 @@ class SqliteReader(AbstractReader):
         # flow.record is quite strict with what is allowed in fieldnames or decriptor name.
         # While SQLite is less strict, we need to sanitize the names to make them compatible.
         table_name_org = table_name
+        table_name_org = table_name_org.replace('"', '""')
         table_name = normalize_fieldname(table_name)
 
         schema = self.con.execute(
@@ -161,8 +162,8 @@ class SqliteReader(AbstractReader):
             fnames.append(fname)
 
         descriptor_cls = RecordDescriptor(table_name, fields)
-        table_name_org = table_name_org.replace("`", r"\\\`")
-        cursor = self.con.execute(f"SELECT * FROM `{table_name_org}`")
+        table_name_org = table_name_org.replace('"', '""')
+        cursor = self.con.execute(f'SELECT * FROM "{table_name_org}"')
         while True:
             rows = cursor.fetchmany(self.batch_size)
             if not rows:
@@ -193,12 +194,15 @@ class SqliteReader(AbstractReader):
 
 
 class SqliteWriter(AbstractWriter):
-    def __init__(self, path: str, batch_size: str | int = 1000, **kwargs):
+    """SQLite writer."""
+
+    def __init__(self, path: str, *, batch_size: str | int = 1000, **kwargs):
         self.descriptors_seen = set()
         self.con = None
-        self.con = sqlite3.connect(path)
+        self.con = sqlite3.connect(path, isolation_level=None)
         self.count = 0
         self.batch_size = int(batch_size)
+        self.con.execute("BEGIN")
 
     def write(self, record: Record) -> None:
         """Write a record to the database"""
@@ -207,17 +211,23 @@ class SqliteWriter(AbstractWriter):
             self.descriptors_seen.add(desc)
             create_descriptor_table(self.con, desc)
             update_descriptor_columns(self.con, desc)
+            self.flush()
 
         db_insert_record(self.con, record)
         self.count += 1
 
         # Commit every batch_size records
         if self.count % self.batch_size == 0:
-            self.con.commit()
+            self.flush()
+
+    def tx_cycle(self) -> None:
+        if self.con.in_transaction:
+            self.con.execute("COMMIT")
+        self.con.execute("BEGIN")
 
     def flush(self) -> None:
         if self.con:
-            self.con.commit()
+            self.tx_cycle()
 
     def close(self) -> None:
         if self.con:
