5caaaa20c75e559ed60aa0f1648bc3ed1994c092
Miauwkeru <Miauwkeru@users.noreply.github.com>
2024-01-22 12:00:27 +0000

    Make test_rdump_stdin_peek windows compatible

--- tests/test_rdump.py
+++ tests/test_rdump.py
@@ -1,10 +1,14 @@
 import base64
+import gzip
 import hashlib
 import json
 import os
 import platform
+import shutil
 import subprocess
+import sys
 from datetime import timezone
+from pathlib import Path
 from unittest import mock
 
 import pytest
@@ -461,10 +465,7 @@ def test_rdump_headerless_csv(tmp_path, capsysbinary):
     ]
 
 
-def test_rdump_stdin_peek(tmp_path):
-    if platform.system() == "Windows":
-        pytest.skip("No Gzip on Windows")
-
+def test_rdump_stdin_peek(tmp_path: Path):
     TestRecord = RecordDescriptor(
         "test/record",
         [
@@ -480,15 +481,21 @@ def test_rdump_stdin_peek(tmp_path):
         writer.write(TestRecord(count=i, foo="bar"))
     writer.close()
 
-    # Gzip compress records file
-    compress_cmd = ["gzip", "--keep", str(path)]
-    subprocess.check_output(compress_cmd)
-    compressed_path = str(path) + ".gz"
+    # Gzip compress records file (using python)
+    with gzip.GzipFile(path.with_suffix(".records.gz"), mode="wb") as gzip_file:
+        gzip_file.write(path.read_bytes())
+
+    on_windows = platform.system() == "Windows"
+    read_command = "cat" if not on_windows else "type"
 
     # Rdump should transparently decompress and select the correct adapter
-    p1 = subprocess.Popen(["cat", compressed_path], stdout=subprocess.PIPE)
+    # Shell gets used on windows for `type` to be available
+    p1 = subprocess.Popen([read_command, gzip_file.filename], stdout=subprocess.PIPE, shell=on_windows)
+
+    # For windows compatibility we use an absolute path of the rdump executable
+    rdump = shutil.which("rdump", path=Path(sys.executable).parent)
     p2 = subprocess.Popen(
-        ["rdump", "-s", "r.count == 5"],
+        [rdump, "-s", "r.count == 5"],
         stdin=p1.stdout,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -624,48 +631,3 @@ def test_flow_record_invalid_tz(tmp_path, capsys):
 
     # restore DISPLAY_TZINFO just in case
     flow.record.fieldtypes.DISPLAY_TZINFO = flow_record_tz(default_tz="UTC")
-
-
-@pytest.mark.parametrize(
-    "rdump_params",
-    [
-        ["--mode=line-verbose"],
-        ["--line-verbose"],
-        ["-Lv"],
-        ["-w", "line://?verbose=true"],
-        ["-w", "line://?verbose=1"],
-        ["-w", "line://?verbose=True"],
-    ],
-)
-def test_rdump_line_verbose(tmp_path, capsys, rdump_params):
-    TestRecord = RecordDescriptor(
-        "test/rdump/line_verbose",
-        [
-            ("datetime", "stamp"),
-            ("bytes", "data"),
-            ("uint32", "counter"),
-            ("string", "foo"),
-        ],
-    )
-    record_path = tmp_path / "test.records"
-
-    with RecordWriter(record_path) as writer:
-        writer.write(TestRecord(counter=1))
-        writer.write(TestRecord(counter=2))
-        writer.write(TestRecord(counter=3))
-
-    from flow.record.adapter.line import field_types_for_record_descriptor
-
-    field_types_for_record_descriptor.cache_clear()
-    assert field_types_for_record_descriptor.cache_info().currsize == 0
-    rdump.main([str(record_path)] + rdump_params)
-    assert field_types_for_record_descriptor.cache_info().misses == 1
-    assert field_types_for_record_descriptor.cache_info().hits == 2
-    assert field_types_for_record_descriptor.cache_info().currsize == 1
-
-    captured = capsys.readouterr()
-    assert captured.err == ""
-    assert "stamp (datetime) =" in captured.out
-    assert "data (bytes) =" in captured.out
-    assert "counter (uint32) =" in captured.out
-    assert "foo (string) =" in captured.out
