ed2a828dea11c6cea0ed8dcfa22a4d376c360833
Miauwkeru <Miauwkeru@users.noreply.github.com>
2024-04-25 11:12:08 +0000

    Fix tests

--- tests/test_fieldtypes.py
+++ tests/test_fieldtypes.py
@@ -1058,7 +1058,7 @@ def test_command_integration(tmp_path: pathlib.Path) -> None:
         # Test a quoted path
         (r"'c:\path to some exe' /d /a", r"c:\path to some exe", [r"/d /a"]),
         # Test a unquoted path
-        (r"'c:\Program Files\hello.exe'", r"c:\Program Files\hello.exe", [""]),
+        (r"'c:\Program Files\hello.exe'", r"c:\Program Files\hello.exe", []),
         # Test an unquoted path with a path as argument
         (r"'c:\Program Files\hello.exe' c:\startmepls.exe", r"c:\Program Files\hello.exe", [r"c:\startmepls.exe"]),
     ],
