1f4e7117f8581e924e085800b55aa43e7e461e3b
Miauwkeru <Miauwkeru@users.noreply.github.com>
2025-02-17 12:36:51 +0000

    Add ipinterface type to fieldtypes.net (#160)

--- flow/record/fieldtypes/net/__init__.py
+++ flow/record/fieldtypes/net/__init__.py
@@ -1,12 +1,19 @@
 from __future__ import annotations
 
 from flow.record.fieldtypes import string
-from flow.record.fieldtypes.net.ip import IPAddress, IPNetwork, ipaddress, ipnetwork
+from flow.record.fieldtypes.net.ip import (
+    IPAddress,
+    IPNetwork,
+    ipaddress,
+    ipinterface,
+    ipnetwork,
+)
 
 __all__ = [
     "IPAddress",
     "IPNetwork",
     "ipaddress",
+    "ipinterface",
     "ipnetwork",
 ]
 
